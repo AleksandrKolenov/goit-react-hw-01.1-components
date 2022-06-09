9a327297e6f09ff89f1d62dcf74f88dc163af4a9
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-06-09 20:44:20 +0300

    react comp

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import Profile from '../components/social-profile/Profile';
+import profile from '../components/social-profile/user.json';
+
+export default function App() {
+  return <div>...</div>
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { ReactDOM } from "react-dom";
+import App from "./components/App";
+
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
@@ -15,3 +18,4 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
