bc6376a877077c4a9662b641c54042389710567a
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-06-09 20:56:14 +0300

    app

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import Profile from './components/social-profile/Profile'
+import profile from './components/social-profile/user.json';
+
+
--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import Profile from '../components/social-profile/Profile';
-import profile from '../components/social-profile/user.json';
-
-export default function App() {
-  return <div>...</div>
-}
